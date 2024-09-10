34b0bebd7351ea901e1b30ed5eb9a89b11ed451d
LucasclFerreira <lucascosta.ferreira@sou.unifal-mg.edu.br>
2024-09-10 20:31:47 -0300

    feature: llm now responds the questions

---
+++ agent.py
@@ -0,0 +1,116 @@
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain_pinecone.vectorstores import PineconeVectorStore
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentType
+from langchain.tools import tool
+from dotenv import load_dotenv
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+load_dotenv()
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+memory = MemorySaver()  # Agent Memory Checkpointer
+
+vectorstore = PineconeVectorStore.from_existing_index('irc-chatbot', embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever(
+    search_type='mmr',
+    search_kwargs={'k': 5, 'fetch_k': 20, 'lambda_mult': 0.7}
+)
+
+
+# Tools
+@tool
+def get_current_date():
+    """"
+        Use this tool to respond to questions related to time. 
+        Returns the current date in YYYY-mm-dd format.
+    """
+    import datetime
+
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d")
+
+@tool
+def get_insurance_policies_data(query: str): 
+    """
+    Use this tool to query data from a rural insurance policies dataframe.
+
+    Keyword arguments:
+    query -- user's question to be answered by the pandas dataframe agent
+    """
+    df_apolices = pd.read_parquet("./data/psr2016a2021_tratado.parquet")
+    llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
+
+    prefix = f"""Aqui estão os valores possíveis para a coluna "cultura" em formato de lista: {df_apolices.cultura.unique()}\nE aqui estão os valores possíveis para a coluna "evento_preponderante" (que representa o desastre climático que causou o sinistro) em formato de lista: {df_apolices.evento_preponderante.unique()}\nLembre-se que o evento_preponderante "-" significa que não houve sinistro na apólice, ignore tal valor quando for relevante.\n        Lembre que o índice de sinistralidade é "premio_liq" dividido por  "valor_indenizacao"\nLembre que taxa do prêmio é "premio_liq" dividido pelo "limite_garantia".
+    """
+
+    pandas_agent = create_pandas_dataframe_agent(
+        llm=llm,
+        df=df_apolices,
+        prefix=prefix,
+        allow_dangerous_code=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True
+    )
+
+    response = pandas_agent.pick('output').invoke(query)
+
+    return response
+
+@tool
+def get_natural_disasters_data(query: str): 
+    """
+    Use this tool to query data from natural disasters and extreme weather events
+
+    Keyword arguments:
+    query -- user's question to be answered by the pandas dataframe agent
+    """
+    df_desastres = pd.read_csv("./data/desastres_brasil.csv")
+    llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
+
+    prefix = f"""Aqui estão os valores possíveis para a coluna "grupo_de_desastre" em formato de lista: {df_desastres.grupo_de_desastre.unique()}\nE aqui estão os valores possíveis para a coluna "descricao_tipologia" (que representa o desastre climático) em formato de lista: {df_desastres.desastre.unique()}\n
+    """
+
+    pandas_agent = create_pandas_dataframe_agent(
+        llm=llm,
+        df=df_desastres,
+        prefix=prefix,
+        allow_dangerous_code=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True
+    )
+
+    response = pandas_agent.pick('output').invoke(query)
+
+    return response
+
+
+# Instatiating Tools
+retriever_tool = create_retriever_tool(
+    retriever=retriever,
+    name='inmet_docs_retriever',
+    description='Procura e retorna trechos relevantes dos Boletins Agroclimatológicos do Instituto Nacional de Meteorologia (INMET)'
+)
+insurance_tool = get_insurance_policies_data
+disaster_tool = get_natural_disasters_data
+datetime_tool = get_current_date
+
+tools = [retriever_tool, insurance_tool, disaster_tool, datetime_tool]
+
+
+# Agent
+agent = create_react_agent(
+    model=llm,
+    tools=tools,
+    checkpointer=memory
+)
+
+response = agent.invoke({'messages': ('user', 'Qual o número de ocorrências de estiagem no Paraná em 2021 e o número de apólices com sinistro relacionado?')}, config={'configurable': {'thread_id': 'test2'}})
+print(response['messages'][-1].content)
--- app.py
+++ app.py
@@ -1,21 +1,42 @@
+from langchain_core.messages import SystemMessage, HumanMessage
 import streamlit as st
-import time
-
-
-# Functions
-def get_response(user_input):
-    for letter in user_input:
-        yield letter
-        time.sleep(0.05)
+from time import sleep
 
 
 # Session State
 if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [{"role": "ai", "content": "Como posso ajudá-lo?"}]
+    st.session_state.chat_history = [{"role": "ai", "content": "Olá! Eu sou o **IRC Climate Risk Assistant**. Como posso te ajudar?"}]
+
+if "agent_memory" not in st.session_state:
+    st.session_state.agent_memory = {"configurable": {"thread_id": "124940"}}
+
+
+# Functions
+@st.cache_resource
+def instantiate_agent():
+    from agent import agent
+    return agent
+
+def generate_stream_from_response(response):
+    for letter in response:
+        sleep(0.01)
+        yield letter
+
+def get_response(user_input):
+    agent = instantiate_agent()
+
+    system_prompt = """Como um especialista em seguro rural e avaliação de riscos climáticos no Brasil, sua função é fornecer informações precisas sobre a ocorrência de desastres, sinistros por eventos climáticos e boletins climáticos do INMET para vários municípios, estados e regiões em todo o país.\n\nResponda exclusivamente aos tópicos mencionados anteriormente. Se não souber a resposta, responda gentilmente que não consegue ou não pode responder a pergunta."""
+
+    return agent.invoke({"messages": [
+        SystemMessage(content=system_prompt),
+        HumanMessage(content=user_input)
+    ]}, config=st.session_state.agent_memory)
 
 
 # UI
-st.title("IRC: Assistente de Riscos Climáticos")
+st.logo('./img/irbped_logo.png')
+
+st.title("IRC Climate Risk Assistant 🌿🌦️")
 
 
 # Chat
@@ -33,5 +54,7 @@ if user_input := st.chat_input("Faça um pergunta..."):
 
     # AI Response
     with st.chat_message("ai"):
-        response = st.write_stream(get_response(user_input))
+        with st.spinner("Pensando..."):
+            response = get_response(user_input)['messages'][-1].content
+        st.write_stream(generate_stream_from_response(response))
     st.session_state.chat_history.append({"role": "ai", "content": response})
